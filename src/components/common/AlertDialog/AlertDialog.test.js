8589990c60c4f75ce34048d3480ed27e9840a11b
bloo0178 <bloo0178@gmail.com>
2019-02-19 12:40:20 -0500

    Refactoring code. Updating project structure and tests.

--- src/components/common/AlertDialog/AlertDialog.test.js
+++ src/components/common/AlertDialog/AlertDialog.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import AlertDialog from '../AlertDialog';
+import AlertDialog from './AlertDialog';
 
 describe("<AlertDialog />", () => {
 
